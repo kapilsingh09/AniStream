4a59011823f33e2468e2f8cf8ab6c41a08de1135
kapilsingh09 <karan404katyura@gmail.com>
2025-07-01 21:53:14 +0530

    simple anime banner name change to GhIbli banner

--- Frontend/src/Home/Homepage.jsx
+++ Frontend/src/Home/Homepage.jsx
@@ -3,7 +3,7 @@ import AnimeSlider from './AnimeSlider';
 import AnimeSection from './AnimeSection';
 import AnimeBanner from './AnimeBanner';
 import SectionComponentKitsu from './SectionComponentKitsu';
-import SimpleAnimeBanner from './SimpleAnimeBanner';
+import GhibliMovieBanner from './GhibliBanner';
 
 import { 
   FetchUpcomingAnime,
@@ -28,31 +28,31 @@ const Homepage = () => {
       <AnimeSlider />
 
       {/* Sections with meaningful titles */}
-      <AnimeSection title="🔥 Top Anime of 2025" fetchFunction={FetchTopAnime} />
+      <AnimeSection title="Top Anime of 2025" fetchFunction={FetchTopAnime} />
       
-      <AnimeSection title="🎯 Upcoming Releases – 2025" fetchFunction={FetchUpcomingAnime} />
+      <AnimeSection title="Upcoming Releases – 2025" fetchFunction={FetchUpcomingAnime} />
 
-      <SectionComponentKitsu title="🌟 Trending Now – Kitsu Picks" fetchFunction={fetchTrendingAnime} />
+      <SectionComponentKitsu title="Trending Now – Kitsu Picks" fetchFunction={fetchTrendingAnime} />
 
       <div className="flex items-center justify-center">
         <AnimeBanner />
       </div>
 
-      <AnimeSection title="🏆 All-Time Top Rated" fetchFunction={FetchTopRatedAnime} />
+      <AnimeSection title="All-Time Top Rated" fetchFunction={FetchTopRatedAnime} />
 
-      <SectionComponentKitsu title="💖 Romantic Drama" subtitle="Suggested by Anime-X" fetchFunction={fetchRomanceAnime} />
+      <SectionComponentKitsu title="Romantic Drama" subtitle="Suggested by Anime-X" fetchFunction={fetchRomanceAnime} />
 
       <div className="flex items-center justify-center">
-        <SimpleAnimeBanner />
+        <GhibliMovieBanner />
       </div>
 
-      <SectionComponentKitsu title="👻 Chilling Horror & Dark Themes" fetchFunction={fetchHorrorAnime} />
+      <SectionComponentKitsu title="Chilling Horror & Dark Themes" fetchFunction={fetchHorrorAnime} />
 
-      <AnimeSection title="📅 Current Season Highlights" fetchFunction={FetchCurrentSeasonAnime} />
+      <AnimeSection title="Current Season Highlights" fetchFunction={FetchCurrentSeasonAnime} />
 
-      <AnimeSection title="💘 Trending Romance & Comedy" fetchFunction={FetchTrendingRomanceComedyAnime} />
+      <AnimeSection title="Trending Romance & Comedy" fetchFunction={FetchTrendingRomanceComedyAnime} />
 
-      <SectionComponentKitsu title="⚔️ Action & Adventure Picks" fetchFunction={fetchActionAnime} />
+      <SectionComponentKitsu title="Action & Adventure Picks" fetchFunction={fetchActionAnime} />
 
       {/* Optional: More banners */}
       <div className="flex items-center justify-center">
