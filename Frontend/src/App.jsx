d13a78c6417ce36f9c990639258a65819191f767
kapilsingh09 <karan404katyura@gmail.com>
2025-07-14 17:25:13 +0530

    smoothbehaviour added on the scrolltotop file in the utils

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,7 @@
+// src/App.jsx
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import ScrollToTop from './utils/ScrollToTop'; 
 
 import Layout from './Layout/Layout';
 import HomePage from './Home/Homepage';
@@ -7,37 +9,37 @@ import ExplorePage from './pages/ExplorePage';
 import JikhanAnimeCard from './routes/JikhanAnimeCard';
 import KitsuAnimeCard from './routes/KitsuAnimeCard';
 import SorryCard from './utils/SorryCard';
-import Demo from './utils/Demo'
-// import LoginPage from './pages/LoginPage';
+import Demo from './utils/Demo';
 import Login from './routes/Login';
-import SearchPage from './pages/SearchPage';
-import KeywordPage from './pages/KeywordPage'
 import Register from './routes/Register';
+import SearchPage from './pages/SearchPage';
+import KeywordPage from './pages/KeywordPage';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        
-        {/* Auth routes */}
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+    <>
+      <ScrollToTop /> {/* ✅ Add this here */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          
+          {/* Auth */}
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
 
-        {/* Other nested routes */}
-        <Route path="/explore" element={<ExplorePage />} />
-        <Route path="/search" element={<SearchPage />} />
-        
-        <Route path="play/:id" element={<JikhanAnimeCard />} />
-        <Route path="/find" element={<JikhanAnimeCard />} />
+          {/* Pages */}
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/play/:id" element={<JikhanAnimeCard />} />
+          <Route path="/find" element={<JikhanAnimeCard />} />
+          <Route path="/kitsu/:id" element={<KitsuAnimeCard />} />
+          <Route path="/genres/:id" element={<Demo />} />
 
-        <Route path="kitsu/:id" element={<KitsuAnimeCard />} />
-        <Route path="genres/:id" element={<Demo />} />
-
-        {/* 404 fallback route */}
-        <Route path="*" element={<SorryCard />} />
-      </Route>
-    </Routes>
+          {/* 404 */}
+          <Route path="*" element={<SorryCard />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
